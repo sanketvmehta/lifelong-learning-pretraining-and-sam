186be41bcb1826c29b64396cd4eb06e847b28f23
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 17:06:19 +0000

    Adding tarfile member sanitization to extractall()

--- img_exps/data/pt_data.py
+++ img_exps/data/pt_data.py
@@ -276,7 +276,26 @@ def get_not_mnist(dataset_location, transform):
     """
     tar_path = os.path.join(dataset_location, "notMNIST_small.tar.gz")
     with tarfile.open(tar_path) as tar:
-        tar.extractall(dataset_location)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, dataset_location)
     dataset = torchvision.datasets.ImageFolder(
         os.path.join(dataset_location, "notMNIST_small"),
         transform=transform,
